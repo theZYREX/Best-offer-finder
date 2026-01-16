5c337c74d0eabd1da528785ab9d1963f139e8b42
fathumans <nightvate@gmail.com>
2026-01-17 01:00:49 +1000

    front-end

---
+++ BOF-WS/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar.tsx";
+import Demo from "./components/Demo.tsx";
+
+function App() {
+  return (
+      <>
+          <NavBar />
+          <Demo/>
+      </>
+  )
+}
+
+export default App
