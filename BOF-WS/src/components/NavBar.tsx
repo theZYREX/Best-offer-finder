3b70d8d8ea2ad27a5f375b446bb650500bdda425
fathumans <nightvate@gmail.com>
2026-01-17 19:13:36 +1000

    update

--- BOF-WS/src/components/NavBar.tsx
+++ BOF-WS/src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 function NavBar() {
     return(
-        <header className="mb-1 mt-3">
+        <header className="mt-3">
             <div className="container-fluid d-grid  align-items-center">
                 <div className="d-flex align-items-center justify-content-center">
-                    <form className="col-9 col-lg-5 mb-5 mb-lg-3" role="search">
-                        <input type="search" className="form-control form-control-dark text-bg-dark rounded-4 shadow-none" placeholder="Поиск" aria-label="Search"/>
+                    <form className="col-9 col-lg-5 mb-3 mb-lg-3" role="search">
+                        <input type="search" className="form-control form-control-dark text-bg-dark rounded-4 shadow-none" placeholder="Поиск" aria-label="Search" />
                     </form>
                 </div>
             </div>
