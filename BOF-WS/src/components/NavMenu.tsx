ee8fc147a2f88572e5d5b1cf3b52cf0ac8777ca2
fathumans <nightvate@gmail.com>
2026-01-19 00:21:34 +1000

    update

---
+++ BOF-WS/src/components/NavMenu.tsx
@@ -0,0 +1,10 @@
+function NavMenu() {
+    return (
+        <footer className="footer mt-auto py-3 bg-body-tertiary">
+            <div className="container">
+                <span className="text-body-secondary"> Test 11111111111111111111 </span>
+            </div>
+        </footer>
+    )
+}
+export default NavMenu;
